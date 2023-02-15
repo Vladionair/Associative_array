8a200aa6a8d1d7dc89290125999dec1cd05fe9ba
Vladionair <45149849+Vladionair@users.noreply.github.com>
2023-02-15 11:31:33 +0300

    open

--- Associative_array.py
+++ Associative_array.py
@@ -1,1 +1,84 @@
 gAAAAABfUT_pBB8LxGG5BdVI5l-n5s1nSwLCCECoDjWQX5Ym8KI_iwb6R_BP_2Tau_y_Czrts2ubkxSkCB9P8uCkTDPfEx3Aw4EMLOJH99UPR8jMBHKNRxiugkG9fn2pDj8BjIpolppkoMiK8A5vEIXdUn_4u8mXToI-4gA4JqyamzxRLzJIKHXZ2unHFuSay8ugE-9QqmScOv_xMttiuES_ELtGJtHaodlNaRJZBLIIXWY6n7CwVC7odCtKW9c0Ub_BCuTOKfcRSqrVrIDrzHGm6dEB-p-CBucGI2bShwaz76pb1Y56Kh0gMcxW5Kk8fJLTa382KBwn6LdUR6iTx-d8-Z_djKfSiPmCz7uxJGaTVUlV523LpSXaGCWj5PscWYGYfTheliClv32OPBn6PpSbfw31T09OsWrIV-SV7rqidLZGxp4Or9Ud5pSOreCZO9_kGqPsyNvfXM3ZY5s0avCe_bhQ9x_-tJc34gESBLGwqg2SIsGYEPAzWFTkAYMLIzeXv4tTgNi1lCS0K13ywF8urnd8IzsX5T5bpfFH_wfNQjxrT1eFQ4rPdzY2LtsNFbGJlHR9yyjA14Xinn_H-JqKe8KbohPYfkX9Jy7qZF68SttpUrMAa7dsHYnF_kXGwfOOHJPtRD_CtJ9bh7K3JpQ8EvKuhtjnlrE-ZWcElNaWmZ2Ammm19Vi3B4E8UuG7bs1tSHsDlWQgWpkALDcu54VhRQ-9pVfsYdtJG_Lk2ULDqFdUjdRsnsB-VaHxaXmrazbQX34s_kpoX312VXMOJjbvtQxuxF_cJS1DFgcoahEf2SFmna7WINeCjGOhd-GvsPv8uT5ikyVUh67jGoFKtRspxf6r-iUQWfY9dbiFp144urcfCVJwf59ACBRK0zfx1BlY45bL0wFpK9MjW2f6yP2ENv71dDOmGWZIgwidn4FnJRM8aU_DSwi3ilhb6C2SFvu4wNulHPIoMCx3ZEBScCrBNEDaQD67COQRisxZuGrpALujGCyme3-MpJJYkUXR1xdiqBuVQ15mS07EDVZJadRMJApSf-rD6rh1zFtz0ag3NZQ-oqtKG_z_QSWgHSHbkh-6sNXJ23wSBVG5zbr3RQX9XuxHWOqXuaTXI6GFKRJ_URRjVaaanlisE8qYQCnUdMUOXhZVlImdn-wVmLG5L8WNGBR1_ZhdwDH0wj9lyBouzDF9arKG7iFxnbFRg0kz9pEXMzyKst0Qj3EqYQnJCeUTsYHx4D5cmjnyqcbQidcqdlZlHj7GjyCc9_Xvt5BpTNiZhDAs_kcMnYQtT-RlwLUe-jVnroMHePfW4bdMYoflZcMMUqlUcu4CvNTUHOJkqExjgZZ6rTzfSj9KdfUUx1N_3eEOf-dFB1hZo5pLfH8fFDUe7BWS-PZMHZ07zL7SDj9Mp3QPfIyky3nQrQiqIq7yV77eSlg7Ugp3VUOUR8wnF-4buX3luvgPHsbZ5X2kYmOw1bvL2t7iubP1VInroD1Stsp8sh6LfsZXt0ndqxHp2YAile8--2B69FcOuq02x8PDUSZ6vZiUk7H4gobx_bwJDanxWiqqCo1wRi4OVeQAI_IQNXRexsrRvAh8WIiCpP3Th3ChuM8qPHe60ea79KgsbmFB-ldLQqh9QSGJNrC8C3t2859ng60XvzdIBF57o1LLbalFtlL7hgE2fU1l36pJxeFRIoKr9AikPHK2g_7Rn7zGEZnQer-DwjfOmvzGOSn33b5p9wok7V3n8Gmj3hCBP3QiIWYeDZlcIgYaC8bueDiLeZKrxFL8xPHo-qJVA7f5eIF0LlaT5Qvfhj_neqHsaics4HNhvSCKuFAl34MlaqooZMPTl6yPfSE5ocisqupmDyctyyN2iJRMeFtGury51qBYqbrXoff_xQqec0NikH2HdX9JH58mOCEoewZ-OGKe5yFqKYTg-mbrpYVxKtBay_E6hVtDkuaMq9bXHhHHDAbiBoCHJbxp3-qTuoad_FBCM8h6MdOhowHnndtaB-1ZBX0b9MzSlk9-GAhjP6-lOQR-lh3Af-wQ_oBIcdItAjwqhEcv9cUxK0HnoOE6cZYxoDUi02vb7oHh0NFbv2tIagLxfmAalUSaduHJEbooW2iVSAcYV8QfXV24Nl5ySQdIS3di1JELYYVa2ftG4FoRsu1g8hFSaHT3rrEPChaHTi2bVCu1sOn-O3Vk3p0YUOhKhahk0vwf1NrArIQrKuwvHlaeAiKt2QybuBkW_Ey3qlAWHGLvOYNk7WTVRQKvtUbM4QK8nIA4v7C_SN7uu26omiEsLCwrz1YuCbuk9X8OFO9cezA7w5Q6A4qon27eu5zIN5PDpOpba9f7H98zJF0zSeK3FXHePceLJnVZOkw1Q9TV6CbffA29-qK21J298HltSdYG9x1s2xtXpEzx6lJv-y_0Ref0YL2DhRbb892voYUBuL-EOgXSZExOqWPRt_lF0qWnBsg_pU-Sm-UOurgAnneY6LJnvK4AkIt_rFbVuy4k2okOe2erPnFomuCb3cWjFl-x7SHP4zIhb6BuAHoUHcqqs9IW0smLkv5nNGGhRHec8wchwKgmdHGKGyW1YWX5L_h_SsFKO_cT3-zOzKl3w3aXGr7IL6ypzCwr-vDjY-_G8QTKOvQSdj30vF_ijGoMMJZaivJvivpWvz2BYBFG0nseCVQV9_tXomrDFEuK7HHwOyGEOTBn1Rt--f0RBEVsSRwWGakLxfLYWulfpC50QSYB1wY-npp0xt22FMDzyMSgJRn6DW5tpbQDz6UOO2eaQmuF8qtaVeA4Apb2I8yxegLfSKJ7854=
+
+class NativeDictionary:
+
+    def __init__(self, sz):
+
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+
+    def hash_fun(self, key):
+
+        if None in self.slots:
+            return self.slots.index(None)
+        else:
+            return None
+
+    def is_key(self, key):
+
+        if key in self.slots:
+            return True
+        else:
+            return False
+
+    def put(self, key, value):
+
+        if key in self.slots:
+            self.values[self.slots.index(key)] = value
+        else:
+            i = self.hash_fun(key)
+            if i is not None:
+                self.slots[i] = key
+                self.values[i] = value
+            else:
+                return None
+
+    def get(self, key):
+
+        if key in self.slots:
+            return self.values[self.slots.index(key)]
+        else:
+            return None
+
+import unittest
+
+class Test_NativeDictionary(unittest.TestCase):
+
+    def test_is_key(self):
+
+        for i in range(len(test.slots)):
+            test.put(i, i + 1)
+        for j in range(len(test.slots)):
+            self.assertEqual(j, test.slots[j])
+        for k in range(len(test.slots)):
+            self.assertNotEqual(k + 10, test.slots[k])
+
+    def test_put(self):
+
+        for i in range(len(test.slots)):
+            test.put(i, i + 1)
+            self.assertEqual(i, test.slots[i])
+            self.assertEqual(i + 1, test.values[i])
+        for j in range(len(test.slots)):
+            test.put(j, j + 2)
+            self.assertEqual(j, test.slots[j])
+            self.assertEqual(j + 2, test.values[j])
+
+    def test_get(self):
+
+        for i in range(len(test.slots)):
+            test.put(i, i + 1)
+        for j in range(len(test.slots)):
+            self.assertEqual(test.get(j), j + 1)
+        for k in range(len(test.slots)):
+            self.assertEqual(test.get(k + 10), None)
+
+    def tearDown(self):
+
+        test.__init__(10)
+
+if __name__ == '__main__':
+  
+    test = NativeDictionary(10)
+    unittest.main(verbosity=2)
